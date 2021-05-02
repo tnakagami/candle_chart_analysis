5fa08ec765cc95ccfe685c0622373996af003407
nas user <pi@nas.localnet.jp>
2021-05-02 21:34:39 +0900

    不要なimportを削除。

--- analysis/src/learning.py
+++ analysis/src/learning.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import torchvision.models as models
 from torch.autograd.function import Function
 import time
